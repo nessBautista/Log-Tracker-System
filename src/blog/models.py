ec2319259c2f979f7b6a240e76a658953770cbdb
Nestor Javier Hernandez <nestorjavierhernandez@Nestors-MacBook-Pro.local>
2020-09-19 11:52:02 -0500

    LTS-1 Build base project:     -Installed requirements.txt file     -Added initial models: Tag, Post, NewsLink, Startup     -Made basic customizations to admin module

---
+++ src/blog/models.py
@@ -0,0 +1,23 @@
+#BLOG
+from datetime import date
+from django.db import models
+from organizer.models import Startup, Tag
+
+class Post(models.Model):
+    title = models.CharField(max_length=63)
+    slug = models.SlugField(max_length=63)
+    text = models.TextField()
+    pub_date = models.DateField()
+    # it has a many to many field with Tags and Startups
+    tags = models.ManyToManyField(Tag)
+    startups = models.ManyToManyField(Startup)
+
+    class Meta:
+        get_latest_by = "pub_date"
+        # orders by publication date descending, then by title ascending
+        ordering = ["-pub_date", "title"]
+        verbose_name = "blog post"
+
+    def __str__(self):
+        date_string = self.pub_date.strftime("%Y-%m-%d")
+        return f"{self.title} on {date_string}"
